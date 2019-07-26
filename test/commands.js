5663711fd085ec70fb8118830ae2a30d725f494c
roliod <rolandoduberu@gmail.com>
2019-07-27 00:34:30 +0200

    just for consistency

--- test/commands.js
+++ test/commands.js
@@ -1,12 +1,12 @@
-var assert = require('assert');
-var commands = require('../src/config/commands.json');
+let assert = require('assert');
+let commands = require('../src/config/commands.json');
 
 describe('Commands', function() {
   describe('includes()', function() {
     it('should have the proper command list structure', function() {
-      var commandList = commands.list;
+      let commandList = commands.list;
       for (key in commandList) {
-        var commandListKeys = Object.keys(commandList[key]);
+        let commandListKeys = Object.keys(commandList[key]);
         assert.equal(commandListKeys.includes('shortcut', 'description'), true);
       }
     });
