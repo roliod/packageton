bd7edf63c82288da4c9a9b7a95a3332fa2c70394
roliod <rolandoduberu@gmail.com>
2019-09-13 00:10:47 +0200

    travis

--- test/list.js
+++ test/list.js
@@ -5,7 +5,6 @@ describe('List Command', function() {
   describe('list()', function() {
     it('should be able to list all the available skeletons', function() {
       let response = shell.exec('./src/cli.js list');
-
       assert.include(response, 'Available skeletons:');
     });
   });
