bf6a1602996fc51006397d67bb1cca6f813aaad9
Roland Oduberu <rolandoduberu@gmail.com>
2021-01-02 23:30:10 +0100

    added tests

--- test/create.js
+++ test/create.js
@@ -13,4 +13,14 @@ describe('Create Command', function() {
       })
     });
   });
+  describe('create()', function() {
+    it('should be able to create a skeleton with a specific folder name', function() {
+      let res = shell.exec('./src/cli.js create php -f myfolder', {async:true});
+      res.stdout.on('data', function(data) {
+        assert.isTrue(fs.existsSync('myfolder'));
+        assert.include(data, 'Done');
+        shell.rm('-rf', 'myfolder');
+      })
+    });
+  });
 });
